f3db3e50784339fdb751f84260bdab89b76d3f92
greenleaf-yay <greenleaf.yay@gmail.com>
2018-11-20 16:35:08 +0900

    테스트

---
+++ sample02/test.js
@@ -0,0 +1,40 @@
+// var name = 'Jun';
+
+// if (name == 'jun') {
+//     console.log("My name is jun.");
+// } else if (name == 'Jun') {
+//     console.log("My name is Jun.");
+// }
+
+// for (var i = 0; i < 5; i++)
+// {
+//     console.log("i = %d", i);
+// }
+
+// let time1 = 100
+// while (time1 > 100)
+// {
+//     console.log("Result: %d", time1);
+//     time1--;
+// }
+
+// let time2 = 
+// do {
+//     console.log("Result: %d", time2);
+//     time2--;
+// } while (time2 > 100)
+
+// switch
+
+let action = "eat";
+switch(action)
+{
+    case 'draw':
+        console.log('draw');
+        break;
+    case 'eat':
+        console.log('eat');
+        break;
+    default:
+        console.log('nothing');
+}

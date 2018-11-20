3e59452cf6baa084b3bd82fd6f5eecf063c9d33b
greenleaf-yay <greenleaf.yay@gmail.com>
2018-11-20 15:57:47 +0900

    181120_jstutorial

---
+++ sample01/object.js
@@ -0,0 +1,19 @@
+var obj = new Object(); //빈객체 생성 1
+
+var obj = {};//빈객체 생성 2
+obj.name = "Junil";
+console.log(obj.name)
+
+var obj2 = new Object();
+obj2["name"] = "홍길동";
+console.log(obj2);
+
+
+var person = {
+    name: "홍길동",
+    age: 33,
+    address: {
+        zipcode: 14500,
+        city:"서울"
+    }
+}
